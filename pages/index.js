d0bbcf1210ba1cb71f4e28c3a3c3810d194bc42a
forchoon <75929797+forchoon@users.noreply.github.com>
2026-04-20 08:11:30 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -456,6 +456,7 @@ export default function Home(){
         <meta name="description" content="그때 그 주식을 샀더라면 지금 얼마일까? 주식 수익률 시뮬레이터"/>
         <meta property="og:title" content="껄무새 - 그때 살걸! 그때 팔걸!"/>
         <meta property="og:description" content="그때 그 주식을 샀더라면 지금 얼마일까?"/>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦜</text></svg>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
